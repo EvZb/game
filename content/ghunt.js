04eab4f5f087427bf698400f35494213bba4efc3
EvZb <130612260+EvZb@users.noreply.github.com>
2023-05-16 08:09:14 -0700

    Update ghunt.js

--- content/ghunt.js
+++ content/ghunt.js
@@ -1,4 +1,4 @@
-(function() {let $1;let $2;let $3;if($ep>1){time('0:0:0:15')
+(function() {let $1;let $2;let $3;if($ep>0){time('0:0:0:15')
 if($tmp>0){$1=['Dark','Water','Ice','Light','Fire','Metal'];let $2=rng(6);let $3=$1[$2-1];if($2==$tmp||$2==$tmp-1||$2==$tmp+1||$2==$tmp-5||$2==$tmp+5){$t=`You find evidence that the ghost may be a ${pic($3)} ${$3} ghost.`}else{$t=`You find evidence that the ghost may <font color="yellow">not</font> be a ${pic($3)} ${$3} ghost.`}}
 if($tmp=='none'){$tmp='tmp';$n=`${btn("investigate","next('ghunt')")}`;$t=`You have been hired to investigate a dangerous ghost. If you can guess the correct type, the client will pay you after exorcising it.`}
 if($tmp>0){$n=`${btn("investigate","next('ghunt')")} | ${btn("guess","if($tmp==ELM('gguess').value){$tmp=0}else{$tmp=-1};next('ghunt')")} ${slist("gguess","Dark:1,Water:2,Ice:3,Light:4,Fire:5,Metal:6")}`}else{if($tmp=="tmp"){$tmp=rng(6)}};
