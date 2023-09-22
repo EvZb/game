aa805fac8c96c8ca068609f155945e7799d0c8ae
EvZb <130612260+EvZb@users.noreply.github.com>
2023-09-22 07:32:22 -0700

    Delete content/start.js

--- content/start.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    if ($tmp == "none") {
-        $tmp = "bruh"
-        $t = `"Welcome to ${col("red", "UNNAMED GAME") + " " + col("lightyellow", "The general gameplay is still being developed.")} In this text based game, you can explore the world and improve your character through experiences`
-        $n = `${btn("next", "next('start')")}`; ELM("intro").remove()
-    } else { $tmp = "none"; $n = `${btn('next', "next('vill')")}`; $t = `You awaken near a village without modern technology.` }
-    end()
-})()
